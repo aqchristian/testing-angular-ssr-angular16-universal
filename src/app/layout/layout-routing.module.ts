737d3d614a2202145fc46fa4313e88d8c7fad35e
Christian <calvarado@sedix.es>
2024-10-11 08:25:47 +0200

    correcciones en el modo no javascript

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
-import { UserListComponent } from '../features/landing-page/user-list/user-list.component';
-import { PostListComponent } from '../features/landing-page/post-list/post-list.component';
-import { AlbumListComponent } from '../features/landing-page/album-list/album-list.component';
-import { LandingPageComponent } from '../features/landing-page/landing-page/landing-page.component';
 
 const routes: Routes = [
   {
